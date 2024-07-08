fb669ae307cc2cdb081ccd1da033f86696f70aa8
Troy Silva <troyjonsilva@gmail.com>
2024-07-08 13:00:17 -0700

    added mobile menu

---
+++ src/app/components/MobileMenu.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function MobileMenu() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	return (
+		<div className="">
+			<button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <svg
+					aria-hidden="true"
+					fill="none"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					className="w-8 h-8 text-white"
+				>
+					<path
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</svg>
+			</button>
+			<div
+				data-isMenuOpen={isMenuOpen}
+				className="fixed top-14 right-0 transition-all duration-300 translate-x-full data-[isMenuOpen=true]:-translate-x-0 bg-white text-black rounded-l-lg shadow-lg"
+			>
+				<ul className="flex flex-col space-y-4 py-4">
+					<li>
+						<Link href="/" className="hover:underline p-4">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link href="/forums" className="hover:underline p-4">
+							Forums
+						</Link>
+					</li>
+					<li>
+						<Link href="/messages" className="hover:underline p-4">
+							Messages
+						</Link>
+					</li>
+					<li>
+						<Link href="/about" className="hover:underline p-4">
+							About Us
+						</Link>
+					</li>
+					<li>
+						<Link href="/profile" className="hover:underline p-4">
+							Profile
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,46 @@
-import Link from "next/link"
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+
 export default function Header() {
-  return (
-    <header className="bg-green-600 p-4 text-white">
-        <nav className="container mx-auto flex justify-between items-center">
-            <h1 className="text-2xl font-bold">GNSQ</h1>
-            <ul className="flex space-x-4">
-                <li><Link href="/" className="hover:underline">Home</Link></li>
-                <li><Link href="/forums" className="hover:underline">Forums</Link></li>
-                <li><Link href="/messages" className="hover:underline">Messages</Link></li>
-                <li><Link href="/about" className="hover:underline">About Us</Link></li>
-                <li><Link href="/profile" className="hover:underline">Profile</Link></li>
-            </ul>
-        </nav>
-    </header>
-  )
+	return (
+		<header className="bg-green-600 p-4 text-white overflow-hidden">
+			<nav className="container mx-auto flex justify-between items-center">
+				<h1 className="text-2xl font-bold">GNSQ</h1>
+				<div>
+					<div className="hidden sm:block">
+						<ul className="flex space-x-4">
+							<li>
+								<Link href="/" className="hover:underline">
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link href="/forums" className="hover:underline">
+									Forums
+								</Link>
+							</li>
+							<li>
+								<Link href="/messages" className="hover:underline">
+									Messages
+								</Link>
+							</li>
+							<li>
+								<Link href="/about" className="hover:underline">
+									About Us
+								</Link>
+							</li>
+							<li>
+								<Link href="/profile" className="hover:underline">
+									Profile
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className="block sm:hidden">
+						<MobileMenu />
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
 }
