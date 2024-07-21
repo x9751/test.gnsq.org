69098cd7fbbeecfe9906ceab5f08fbacbb48628b
Troy Silva <troyjonsilva@gmail.com>
2024-07-21 16:12:57 -0700

    footer copyright year changed to dynamic

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className="bg-green-600 p-4 text-white text-center mt-auto">
-			<p>&copy; 2024 GNSQ. All rights reserved.</p>
+			<p>&copy; {new Date().getFullYear()} GNSQ. All rights reserved.</p>
 			<ul className="flex justify-center space-x-4">
 				<li>
 					<Link href="/contact" className="hover:underline">
