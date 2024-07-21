b6d6ef66b04d4f813ed3eb54ff633efb31240598
Troy Silva <troyjonsilva@gmail.com>
2024-07-21 13:55:44 -0700

    added loading status to forum thread comment

--- src/app/forums/thread/[thread_id]/Reply.tsx
+++ src/app/forums/thread/[thread_id]/Reply.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import BBCodeEditor from "@/app/components/BBCode";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 import { replyToThread } from "./actions";
 
 export default function ReplyThread({ thread_id }: { thread_id: number }) {
@@ -14,17 +14,25 @@ export default function ReplyThread({ thread_id }: { thread_id: number }) {
 			<form className="flex gap-4 border-t p-4" action={formAction}>
 				<input type="hidden" name="thread_id" value={thread_id} />
 				<BBCodeEditor name="content" placeholder="Reply to thread..." />
-				<button
-					type="submit"
-					className="bg-blue-500 hover:bg-blue-7000 text-white font-bold py-2 px-4 rounded"
-				>
-					Reply
-				</button>
+				<SubmitButton />
 			</form>
 			{formState.error && <p className="text-red-500">{formState.error}</p>}
 			{formState.message && (
 				<p className="text-green-500">{formState.message}</p>
 			)}
 		</>
+	);
+}
+
+function SubmitButton() {
+	const { pending} = useFormStatus();
+	return (
+		<button
+			type="submit"
+			disabled={pending}
+			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		>
+			{pending ? "Replying..." : "Reply"}
+		</button>
 	);
 }
